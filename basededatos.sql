7371ec60238f07d34d52e480c36c9f72fc69b449
binarycrc@gmail.com <binarycrc@gmail.com>
2020-05-02 11:29:43 -0600

    main listo

---
+++ basededatos.sql
@@ -0,0 +1,82 @@
+﻿USE [master]
+GO
+/****** Object:  Database [SegundoOrdinario00830]    Script Date: 5/2/2020 10:18:23 AM ******/
+CREATE DATABASE [SegundoOrdinario00830]
+
+USE [SegundoOrdinario00830]
+GO
+/****** Object:  Table [dbo].[Alumno]    Script Date: 5/2/2020 10:18:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Alumno](
+	[Id_alumno] [int] NOT NULL,
+	[Dsc_nombre] [varchar](50) NOT NULL,
+	[Dsc_apellido1] [varchar](50) NOT NULL,
+	[Dsc_apellido2] [varchar](50) NOT NULL,
+	[Dsc_user_name] [varchar](50) NOT NULL,
+	[Dsc_password] [varchar](50) NOT NULL,
+	[Dsc_rol] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Alumno] PRIMARY KEY CLUSTERED 
+(
+	[Id_alumno] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Cursos]    Script Date: 5/2/2020 10:18:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Cursos](
+	[Id_curso] [int] NOT NULL,
+	[Dsc_curso] [varchar](50) NOT NULL,
+	[Dsc_Grado] [varchar](50) NOT NULL,
+	[Can_Horas] [int] NOT NULL,
+ CONSTRAINT [PK_Cursos] PRIMARY KEY CLUSTERED 
+(
+	[Id_curso] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[CursosAlumno]    Script Date: 5/2/2020 10:18:23 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[CursosAlumno](
+	[Id_curso] [int] NOT NULL,
+	[Id_alumno] [int] NOT NULL,
+	[NotaOrdinario1] [decimal](18, 0) NOT NULL,
+	[NotaOrdinario2] [decimal](18, 0) NOT NULL,
+	[NotaProyecto] [decimal](18, 0) NOT NULL,
+ CONSTRAINT [PK_CursosAlumno] PRIMARY KEY CLUSTERED 
+(
+	[Id_curso] ASC,
+	[Id_alumno] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Alumno] ADD  CONSTRAINT [DF_Alumno_Dsc_rol]  DEFAULT ((0)) FOR [Dsc_rol]
+GO
+ALTER TABLE [dbo].[CursosAlumno] ADD  CONSTRAINT [DF_CursosAlumno_NotaOrdinario1]  DEFAULT ((0)) FOR [NotaOrdinario1]
+GO
+ALTER TABLE [dbo].[CursosAlumno] ADD  CONSTRAINT [DF_CursosAlumno_NotaOrdinario2]  DEFAULT ((0)) FOR [NotaOrdinario2]
+GO
+ALTER TABLE [dbo].[CursosAlumno] ADD  CONSTRAINT [DF_CursosAlumno_NotaProyecto]  DEFAULT ((0)) FOR [NotaProyecto]
+GO
+ALTER TABLE [dbo].[CursosAlumno]  WITH CHECK ADD  CONSTRAINT [FK_CursosAlumno_Alumno] FOREIGN KEY([Id_alumno])
+REFERENCES [dbo].[Alumno] ([Id_alumno])
+GO
+ALTER TABLE [dbo].[CursosAlumno] CHECK CONSTRAINT [FK_CursosAlumno_Alumno]
+GO
+ALTER TABLE [dbo].[CursosAlumno]  WITH CHECK ADD  CONSTRAINT [FK_CursosAlumno_Cursos] FOREIGN KEY([Id_curso])
+REFERENCES [dbo].[Cursos] ([Id_curso])
+GO
+ALTER TABLE [dbo].[CursosAlumno] CHECK CONSTRAINT [FK_CursosAlumno_Cursos]
+GO
+USE [master]
+GO
+ALTER DATABASE [SegundoOrdinario00830] SET  READ_WRITE 
+GO
--- frmServidor.Designer.cs
+++ frmServidor.Designer.cs
@@ -28,13 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.txtId_alumno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDsc_nombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDsc_apellido1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDsc_apellido2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(775, 425);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.txtDsc_apellido2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.txtDsc_apellido1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtDsc_nombre);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.txtId_alumno);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(767, 399);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Registro Alumno";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Registro Cursos";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(767, 399);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Registro Notas";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(767, 399);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Validar Alumnos";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // txtId_alumno
+            // 
+            this.txtId_alumno.Location = new System.Drawing.Point(6, 30);
+            this.txtId_alumno.Name = "txtId_alumno";
+            this.txtId_alumno.Size = new System.Drawing.Size(100, 20);
+            this.txtId_alumno.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Identificación";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre";
+            // 
+            // txtDsc_nombre
+            // 
+            this.txtDsc_nombre.Location = new System.Drawing.Point(6, 69);
+            this.txtDsc_nombre.Name = "txtDsc_nombre";
+            this.txtDsc_nombre.Size = new System.Drawing.Size(100, 20);
+            this.txtDsc_nombre.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Primer Apellido";
+            // 
+            // txtDsc_apellido1
+            // 
+            this.txtDsc_apellido1.Location = new System.Drawing.Point(6, 108);
+            this.txtDsc_apellido1.Name = "txtDsc_apellido1";
+            this.txtDsc_apellido1.Size = new System.Drawing.Size(100, 20);
+            this.txtDsc_apellido1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Segundo Apellido";
+            // 
+            // txtDsc_apellido2
+            // 
+            this.txtDsc_apellido2.Location = new System.Drawing.Point(6, 147);
+            this.txtDsc_apellido2.Name = "txtDsc_apellido2";
+            this.txtDsc_apellido2.Size = new System.Drawing.Size(100, 20);
+            this.txtDsc_apellido2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Rol";
+            // 
+            // frmServidor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tabControl1);
+            this.Name = "frmServidor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Servidor";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDsc_apellido2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDsc_apellido1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDsc_nombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId_alumno;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
     }
 }
 
